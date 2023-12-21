5a7c944fcd8a9300696a2a03905812b90a7dd9f7
Andrew Strange <andrew@foodbomb.com.au>
2023-12-22 00:51:01 +1100

    front page theme

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import '@elastic/eui/dist/eui_theme_light.css';
+import { EuiCard, EuiIcon, EuiFlexGroup, EuiFlexItem,EuiPanel } from '@elastic/eui';
 
 export default function LoggedOut() {
   const { login, register } = useKindeAuth();
@@ -7,52 +9,59 @@ export default function LoggedOut() {
     <>
       <header>
         <nav className="nav container">
-          <h1 className="text-display-3">KindeAuth</h1>
+          <h1 className="text-display-2">Internal Skill Search</h1>
           <div>
-            <button className="btn btn-ghost sign-in-btn" onClick={login}>
+            {/* <button className="btn btn-ghost sign-in-btn" onClick={login}>
               Sign in
             </button>
             <button className="btn btn-dark" onClick={register}>
               Sign up
-            </button>
+            </button> */}
           </div>
         </nav>
       </header>
 
       <main>
-        <div className="container">
-          <div className="card hero">
-            <p className="text-display-1 hero-title">
-              Let's start authenticating <br /> with KindeAuth
-            </p>
-            <p className="text-body-1 hero-tagline">Configure your app</p>
 
-            <a
-              href="https://kinde.com/docs/developer-tools/react-sdk"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-light btn-big"
-            >
-              Go to docs
-            </a>
-          </div>
+        <div className="container hero">
+          <EuiFlexGroup gutterSize="l">
+            <EuiFlexItem >
+              <EuiPanel
+                style={{padding: "30px", background: "#ffffff", borderRadius: "10px", boxShadow: "0px 0px 0px"}}
+              >
+                <p className="text-body-1 hero-tagline">Looking for people with skills?</p>
+                <p className="text-body-1 hero-tagline">Have skills and time and would to work on stuff?</p>
+                <div
+                  
+                >
+                  <EuiFlexGroup gutterSize="1"
+                    style={{ margin: "auto"}}
+                  >
+                    <EuiFlexItem 
+                      style={{ margin: "auto"}}
+                    >
+                      <EuiCard
+                        style={{height: "100px", width: "200px", margin: "30px auto", paddingTop: "35px", borderRadius: "30px",background: "linear-gradient(145deg, #fbffff, #d4dbe0)"}}
+                        title={`Sign in`}
+                        isDisabled={false}
+                        onClick={() => {login()}}
+                      />
+                      <EuiCard
+                        style={{height: "100px", width: "200px", margin: "30px auto", paddingTop: "35px", borderRadius: "30px",background: "linear-gradient(145deg, #e4f2fb, #c0cbd4)"}}
+                        title={`Sign up`}
+                        isDisabled={false}
+                        onClick={() => {register()}}
+                      />
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </div>
+              </EuiPanel>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </div>
       </main>
 
       <footer className="footer">
-        <div className="container">
-          <strong className="text-heading-2">KindeAuth</strong>
-          <p className="footer-tagline text-body-3">
-            Visit our{" "}
-            <a className="link" href="https://kinde.com/docs">
-              help center
-            </a>
-          </p>
-
-          <small className="text-subtle">
-            © 2023 KindeAuth, Inc. All rights reserved
-          </small>
-        </div>
       </footer>
     </>
   );
